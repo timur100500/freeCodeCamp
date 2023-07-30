02be80887cb3864b78e8e978307fe9217bb3e97d
timur100500 <nadyrgulov.timur@yandex.ru>
2023-07-30 16:44:54 +0500

    null2

--- 1.js
+++ 1.js
@@ -1,3 +1,5 @@
+const chai = require('chai');
+const assert = chai.assert;
 test('#isNull, #isNotNull', function() {
     assert.isNull(null, 'This is an optional error description - e.g. null is null');
     assert.isNotNull(1, '1 is not null');
